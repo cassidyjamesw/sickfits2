3ad61bf92726580fe0a304e540a302693c9cbbce
James Cassidy <cassidyjamesw@gmail.com>
2019-04-03 15:45:00 -0400

    meta and nav

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,16 @@
 import App, { Container } from "next/app";
+import Page from "../components/Page";
 
 class MyApp extends App {
   render() {
     const { Component } = this.props;
 
     return (
-      <container>
-        <p>Hey i'm on every page</p>
-        <Component />
-      </container>
+      <Container>
+        <Page>
+          <Component />
+        </Page>
+      </Container>
     );
   }
 }
