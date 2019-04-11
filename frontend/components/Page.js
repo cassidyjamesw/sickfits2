06f2d9009eece9e1ef8c1cd573bfdde0b1afd356
James Cassidy <cassidyjamesw@gmail.com>
2019-04-11 18:16:18 -0400

    themes

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -3,14 +3,35 @@ import Header from "./Header";
 import Meta from "./Meta";
 import styled from "styled-components";
 
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
 class Page extends Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
-        {this.props.children}
-      </div>
+        <Inner>{this.props.children}</Inner>
+      </StyledPage>
     );
   }
 }
