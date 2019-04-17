fcc1eb26a0a428000ee7660be6be3bd5a10d7acc
James Cassidy <cassidyjamesw@gmail.com>
2019-04-17 16:50:09 -0400

    hooking up graphQL to react

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -3,12 +3,10 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import styled from "styled-components";
 import Item from "./Item";
-import Pagination from "./Pagination";
-import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
-    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
+  query ALL_ITEMS_QUERY {
+    items {
       id
       title
       price
@@ -35,14 +33,7 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Pagination page={this.props.page} />
-        <Query
-          query={ALL_ITEMS_QUERY}
-          // fetchPolicy="network-only"
-          variables={{
-            skip: this.props.page * perPage - perPage
-          }}
-        >
+        <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
@@ -55,11 +46,9 @@ class Items extends Component {
             );
           }}
         </Query>
-        <Pagination page={this.props.page} />
       </Center>
     );
   }
 }
 
 export default Items;
-export { ALL_ITEMS_QUERY };
